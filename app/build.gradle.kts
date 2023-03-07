5345c9825901c39244c59cbdf785c2067b080aeb
Karol Wąsowski <wasowski02@protonmail.com>
2023-03-07 13:10:11 +0100

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+fun versionCode(): Int {
+    val secondsSinceEpoch = System.currentTimeMillis() / 1000
+    // This will fail eventually, but well… It's the best we have
+    return secondsSinceEpoch.toInt()
+}
+
+android {
+    namespace = "com.pokerio.app"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.pokerio.app"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = versionCode()
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.8.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
