7873d4d57c411f2dbd338da5eae8f08be2a8d7fa
Karol Wąsowski <wasowski02@protonmail.com>
2023-04-25 08:35:08 +0200

    Preparing for Sprint 2 (#15)
    - updated server address
    - added placeholders in lobby (will make work easier later on)

--- app/src/main/java/com/pokerio/app/screens/LobbyScreen.kt
+++ app/src/main/java/com/pokerio/app/screens/LobbyScreen.kt
@@ -1,6 +1,9 @@
 package com.pokerio.app.screens
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -9,6 +12,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,6 +22,7 @@ import com.pokerio.app.utils.GameState
 @Composable
 fun LobbyScreen() {
     var numberOfPlayers by remember { mutableStateOf(0) }
+    val context = LocalContext.current
 
     // Sign-up for updates when a new player appears
     val callbackId = GameState.addOnPlayerJoinedCallback { numberOfPlayers = GameState.players.size }
@@ -29,11 +34,27 @@ fun LobbyScreen() {
     }
 
     // This is a debugging UI
-    Column(modifier = Modifier.testTag("lobby_column")) {
-        Text(text = "Game code: ${GameState.gameID}")
-        Text(text = "Players ($numberOfPlayers):")
-        GameState.players.forEach {
-            Text(text = it.nickname)
+    Column() {
+        Column(modifier = Modifier.testTag("lobby_column")) {
+            Text(text = "Game code: ${GameState.gameID}")
+            Text(text = "Players ($numberOfPlayers):")
+            GameState.players.forEach {
+                Text(text = it.nickname)
+            }
+        }
+        Button(onClick = { updateGameSettings(context) }) {
+            Text(text = "Update game settings")
+        }
+        Button(onClick = { startGame(context) }) {
+            Text(text = "Start game")
         }
     }
+}
+
+private fun updateGameSettings(context: Context) {
+    Toast.makeText(context, "TODO: Update settings", Toast.LENGTH_LONG).show()
+}
+
+private fun startGame(context: Context) {
+    Toast.makeText(context, "TODO: Start game", Toast.LENGTH_LONG).show()
 }
