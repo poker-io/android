d25a152e27f41337f9ad56af0960a9e74a243544
aetn23 <mikolajpirog@gmail.com>
2023-04-30 22:29:14 +0200

    Remove never used files

--- app/src/main/java/com/pokerio/app/screens/Game.kt
+++ app/src/main/java/com/pokerio/app/screens/Game.kt
@@ -1,28 +1,14 @@
 package com.pokerio.app.screens
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.pokerio.app.utils.GameState
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun Game() {
-    val sectionTitleFontSize = 24.sp
-    val sectionTitleFontWeight = FontWeight.Bold
-    val sectionTitleModifier = Modifier.padding(10.dp)
-    val spacerModifier = Modifier.padding(10.dp)
-
     Column() {
         Text(text = "Player cards:")
         Text(text = GameState.card1.toString())
