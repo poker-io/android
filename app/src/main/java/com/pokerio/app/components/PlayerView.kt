ab4c04463948fe55e3636eaefe5c87ba51fb452a
Szymon Potrzebowski <szymonp1806@gmail.com>
2023-06-06 03:33:46 +0200

    added recompose on action

--- app/src/main/java/com/pokerio/app/components/PlayerView.kt
+++ app/src/main/java/com/pokerio/app/components/PlayerView.kt
@@ -1,6 +1,5 @@
 package com.pokerio.app.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -27,14 +26,15 @@ import com.pokerio.app.utils.Player
 import com.pokerio.app.utils.PlayerProvider
 
 val TEXT_MODIFIER = Modifier.padding(2.dp)
-//val NICK_BACKGROUND_COLOR = Color.
+
+// val NICK_BACKGROUND_COLOR = Color.
 val PLAYER_VIEW_CARD_SHAPE =
-        RoundedCornerShape(
-            topStart = 8.dp,
-            topEnd = 8.dp,
-            bottomStart = 8.dp,
-            bottomEnd = 8.dp
-        )
+    RoundedCornerShape(
+        topStart = 8.dp,
+        topEnd = 8.dp,
+        bottomStart = 8.dp,
+        bottomEnd = 8.dp
+    )
 
 @Composable
 @Preview
@@ -45,67 +45,67 @@ fun PlayerView(
         modifier = Modifier.width(100.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Card( shape = PLAYER_VIEW_CARD_SHAPE,
+        Card(
+            shape = PLAYER_VIEW_CARD_SHAPE
         ) {
             Column(
-                modifier = Modifier.padding(1.dp),
+                modifier = Modifier.padding(1.dp)
             ) {
-            Card(
-                modifier = Modifier.width(IntrinsicSize.Max).align(CenterHorizontally)
-            ) {
-                Text(
-                    text = player.nickname,
-                    modifier = TEXT_MODIFIER,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                    color = Color.Black
-                )
-            }
-            Card(
-                modifier = Modifier.width(IntrinsicSize.Max),
-                shape = RectangleShape
-            ) {
-                Text(
-                    text = "Funds",
-                    fontSize = 10.sp
-                )
-            }
-            OutlinedCard(
-                shape = PLAYER_VIEW_CARD_SHAPE,
-                modifier = Modifier.align(CenterHorizontally).fillMaxWidth()
-            ) {
-
-                Text(
-                    text = "${player.funds}",
-                    modifier = Modifier.align(CenterHorizontally),
-                    fontSize = 13.sp
-                )
-            }
-            Card(
-                modifier = Modifier.width(IntrinsicSize.Max),
-                shape = RectangleShape
-            ) {
-                Text(
-                    text = "Bet",
-                    fontSize = 10.sp
-                )
-            }
-            OutlinedCard(
-                shape = PLAYER_VIEW_CARD_SHAPE,
-                modifier = Modifier.align(CenterHorizontally).fillMaxWidth()
-            ) {
-                Text(
-                    text = "${player.bet}",
-                    modifier = Modifier.align(CenterHorizontally),
-                    fontSize = 11.sp
-                )
-            }
-            if (player.folded) {
-                Text(
-                    stringResource(R.string.fold),
-                    color = Color.Gray
-                )
-            }
+                Card(
+                    modifier = Modifier.width(IntrinsicSize.Max).align(CenterHorizontally)
+                ) {
+                    Text(
+                        text = player.nickname,
+                        modifier = TEXT_MODIFIER,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        color = Color.Black
+                    )
+                }
+                Card(
+                    modifier = Modifier.width(IntrinsicSize.Max),
+                    shape = RectangleShape
+                ) {
+                    Text(
+                        text = "Funds",
+                        fontSize = 10.sp
+                    )
+                }
+                OutlinedCard(
+                    shape = PLAYER_VIEW_CARD_SHAPE,
+                    modifier = Modifier.align(CenterHorizontally).fillMaxWidth()
+                ) {
+                    Text(
+                        text = "${player.funds}",
+                        modifier = Modifier.align(CenterHorizontally),
+                        fontSize = 13.sp
+                    )
+                }
+                Card(
+                    modifier = Modifier.width(IntrinsicSize.Max),
+                    shape = RectangleShape
+                ) {
+                    Text(
+                        text = "Bet",
+                        fontSize = 10.sp
+                    )
+                }
+                OutlinedCard(
+                    shape = PLAYER_VIEW_CARD_SHAPE,
+                    modifier = Modifier.align(CenterHorizontally).fillMaxWidth()
+                ) {
+                    Text(
+                        text = "${player.bet}",
+                        modifier = Modifier.align(CenterHorizontally),
+                        fontSize = 11.sp
+                    )
+                }
+                if (player.folded) {
+                    Text(
+                        stringResource(R.string.fold),
+                        color = Color.Gray
+                    )
+                }
             }
         }
     }
