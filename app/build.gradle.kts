641035c07c336c3d878549b0ddf38418b5eb4827
Karol Wąsowski <wasowski02@protonmail.com>
2023-06-08 18:31:04 +0200

    Updated app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 24
         targetSdk = 33
         versionCode = versionCode()
-        versionName = "1.0"
+        versionName = "0.5-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
